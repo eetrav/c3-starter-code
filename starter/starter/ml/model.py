c3cf9589d1e2585308a78adc51310040eac374eb
Emily Travinsky <eberkson@email.arizona.edu>
2023-12-14 14:50:12 -0800

    EDA and model training

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,10 +1,19 @@
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 # Optional: implement hyperparameter tuning.
+
+
 def train_model(X_train, y_train):
     """
-    Trains a machine learning model and returns it.
+    Trains a Logistic Regression model and returns it.
+
+    Constructs a model pipeline to: 
+    1) Performs feature pre-processing to normalize and zero-center 
+    non-categorical variables
+    2) Train a Logistic Regression model with cross-fold validation. 
 
     Inputs
     ------
@@ -12,18 +21,27 @@ def train_model(X_train, y_train):
         Training data.
     y_train : np.array
         Labels.
+
     Returns
     -------
     model
         Trained machine learning model.
     """
 
-    pass
+    # From scikit-learn LogisticRegressionCV documentation:
+    # ‘newton-cholesky’ is a good choice for n_samples >> n_features,
+    # especially with one-hot encoded categorical features with rare categories.
+    pipe = make_pipeline(
+        StandardScaler(), LogisticRegressionCV(solver="newton-cholesky"))
+
+    pipe.fit(X_train, y_train)
+
+    return pipe
 
 
 def compute_model_metrics(y, preds):
     """
-    Validates the trained machine learning model using precision, recall, and F1.
+    Validates the trained ML model using precision, recall, and F1.
 
     Inputs
     ------
@@ -31,30 +49,38 @@ def compute_model_metrics(y, preds):
         Known labels, binarized.
     preds : np.array
         Predicted labels, binarized.
+
     Returns
     -------
     precision : float
     recall : float
     fbeta : float
     """
+
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
     recall = recall_score(y, preds, zero_division=1)
+
     return precision, recall, fbeta
 
 
-def inference(model, X):
-    """ Run model inferences and return the predictions.
+def inference(model, x_test):
+    """
+    Run model inferences and return the predictions.
 
     Inputs
     ------
-    model : ???
+    model : sklearn.linear_model.base.LinearRegressionCV
         Trained machine learning model.
     X : np.array
         Data used for prediction.
+
     Returns
     -------
     preds : np.array
         Predictions from the model.
     """
-    pass
+
+    preds = model.predict(x_test)
+
+    return preds
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,13 +1,21 @@
 # Script to train machine learning model.
+import pandas as pd
 
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+from ml.data import process_data
+from ml.model import train_model, inference, compute_model_metrics
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+census_df = pd.read_csv('../data/census_cleaned.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(census_df, test_size=0.20)
 
 cat_features = [
     "workclass",
@@ -19,10 +27,25 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+# Process the training data with the process_data function.
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
 
-# Train and save a model.
+# Train the model
+model = train_model(X_train, y_train)
+
+# Perform inference on the trained model
+preds = inference(model, X_test)
+
+# Compute model metrics
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+print(precision, recall, fbeta)
