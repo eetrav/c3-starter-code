5759d0b5a8649d02ce902ea8782315a7c51fe57e
Emily Travinsky <eberkson@email.arizona.edu>
2023-12-14 15:36:38 -0800

    Updating filepaths for test data.

--- starter/tests/conftest.py
+++ starter/tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.fixture(scope='session')
 def messy_data():
 
-    df = pd.read_csv("messy_data_sample.csv")
+    df = pd.read_csv("./starter/tests/messy_data_sample.csv")
 
     return df
 
@@ -13,6 +13,6 @@ def messy_data():
 @pytest.fixture(scope='session')
 def clean_data():
 
-    df = pd.read_csv("clean_data_sample.csv")
+    df = pd.read_csv("./starter/tests/clean_data_sample.csv")
 
     return df
