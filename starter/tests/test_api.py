4ab1e5e94869c3cd068e2675524be981708be59b
Emily Travinsky <eberkson@email.arizona.edu>
2024-02-05 22:17:00 -0800

    second test and post test

--- starter/tests/test_api.py
+++ starter/tests/test_api.py
@@ -15,30 +15,23 @@ client = TestClient(app)
 
 def test_get_path():
     r = client.get("/")
-    print(r)
     assert r.status_code == 200
     assert r.json() == {"greeting": "Welcome to our salary prediction model!"}
 
 
-# def test_get_path_query():
-#     r = client.get("/items/42?count=5")
-#     assert r.status_code == 200
-#     assert r.json() == {"fetch": "Fetched 5 of 42"}
+def test_get_malformed():
+    r = client.get("/items")
+    assert r.status_code != 200
 
 
-# def test_get_malformed():
-#     r = client.get("/items")
-#     assert r.status_code != 200
+def test_model_predicts_over_50k(salary_over_50k: Person):
+    """
+    Function to test that expected columns are present in the dataframe.
 
+    Args:
+        preprocessor (PreProcessor): Trained data Preprocessor.
+    """
 
-# def test_model_predicts_over_50k(salary_over_50k: Person):
-#     """
-#     Function to test that expected columns are present in the dataframe.
-
-#     Args:
-#         preprocessor (PreProcessor): Trained data Preprocessor.
-#     """
-
-#     url = "https://reqres.in/api/users"
-#     response = requests.get(url)
-#     assert response.status_code == 200
+    r = client.post("/prediction/", salary_over_50k)
+    print(r)
+    # assert response.status_code == 200
