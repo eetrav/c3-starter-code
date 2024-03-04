7845c144f2f9921456a8cab3347e8070e2878229
Emily Travinsky <eberkson@email.arizona.edu>
2024-03-04 12:32:57 -0800

    use testing pkls for main

--- starter/main.py
+++ starter/main.py
@@ -21,8 +21,8 @@ cat_features = [
 app = FastAPI()
 
 # Load pre-computed encoder and pre-trained model
-encoder = joblib.load("./starter/model/encoder.pkl")
-model = joblib.load("./starter/model/test_model.pkl")
+encoder = joblib.load("./starter/tests/encoder.pkl")
+model = joblib.load("./starter/tests/test_model.pkl")
 
 
 def hyphenize(field: str) -> str:
